451e0ffb552c1a2c2a467a2f8269b9ef7e0625b4
Alejandro Martinez <martinez.ale7722@gmail.com>
2022-03-19 16:07:00 -0300

    "ej 1-2 completos"

--- coleccionesEj1_2/src/coleccionesej1_2/ColeccionesEj1_2.java
+++ coleccionesEj1_2/src/coleccionesej1_2/ColeccionesEj1_2.java
@@ -5,6 +5,10 @@
  */
 package coleccionesej1_2;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
 /**
  *
  * @author Alfa
@@ -15,7 +19,35 @@ public class ColeccionesEj1_2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner leer = new Scanner(System.in);
+        ArrayList<String> razaPerros = new ArrayList<>();
+        String raza;
+        String rta;
+        String razaElim;
+        do {
+            System.out.println("Ingrese raza de Perro: ");
+            raza = leer.next();
+            razaPerros.add(raza);
+            System.out.println("Agregar otro perro, si/no ");
+            rta = leer.next();
+        } while ("si".equalsIgnoreCase(rta));
+        System.out.println(" RAZAS ");
+        System.out.println("");
+        
+        for(String lista : razaPerros){
+            System.out.println(lista);
+        }
+        System.out.println(" TOTAL PERROS: " + razaPerros.size());
+        System.out.println("INGRESE LA RAZA QUE NO DEBE ESTAR EN LA LISTA");
+        razaElim = leer.next();
+        Iterator<String> mi_iterator = razaPerros.iterator();
+        while(mi_iterator.hasNext()){
+            String it = mi_iterator.next();
+            if (it.equals(razaElim)){
+                mi_iterator.remove();
+                System.out.println(razaPerros);
+            }
+        }
     }
     
 }
